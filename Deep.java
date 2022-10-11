206f7f16bf646f19f40bee72686a4ceb4d2718de
jmeegan2 <jmeegan8@gmail.com>
2022-10-10 20:56:33 -0400

    more practice examples of deep/shallow copy and polymorphism

--- Deep.java
+++ Deep.java
@@ -3,7 +3,71 @@ import java.util.Arrays;
 
 public class Deep {
 
+    //instantiate int data array
     private int[] data;
+
+    //method to make deep copy
+    public Deep(int[] values) {
+        data = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            data[i] = values[i];
+        }
+    }
+
+    public void showData(){
+        System.out.println(Arrays.toString(data));
+    }
+
+    public static void main(String[] args) {
+        int[] values = {44,33,22};
+        //new obj for deep copy and deep copy method called
+        Deep deepCopy = new Deep(values);
+        deepCopy.showData();
+        //show that a change to original obj wont affect deep copy obj
+        values[0] = 3314123;
+        deepCopy.showData();
+        System.out.println("Original after change: " + Arrays.toString(values));
+    }
+
+}
+
+//    //instantiate data array to be used later
+//    private int[] data;
+//
+//    //method to make the deep copy
+//    public Deep(int[] values) {
+//        //assign data array same length as values length
+//        data = new int[values.length];
+//        //go through values array and assign same values based on index to corresponding data array
+//        for (int i = 0; i < values.length; i++) {
+//            data[i] = values[i];
+//        }
+//    }
+//
+//        //method to show the data
+//        public void showData() {
+//            System.out.println(Arrays.toString(data));
+//        }
+//
+//    public static void main(String[] args) {
+//        int[] values = {3,7,8};
+//        //make the deep copy by initializing new obj and calling the method
+//        Deep deepCopy = new Deep(values);
+//        //show the data
+//        deepCopy.showData();
+//        //show that change to original obj wont affect new one
+//        values[0] = 23;
+//        deepCopy.showData();
+//    }
+//
+//
+//
+//    }
+
+
+
+    /*
+      private int[] data;
 
     //method that takes in int array values makes a deep copy
     public Deep(int[] values) {
@@ -31,5 +95,7 @@ public class Deep {
         values[0] = 13;
         deepCopy.showData();
     }
+     */
 
-}
+
+
--- Shallow.java
+++ Shallow.java
@@ -1,8 +1,68 @@
-
-
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Shallow {
+
+
+
+        //code from top of head under 5 mins to make one mistake which was not passing values in method parameter
+
+    //int array declared
+    static private int[] data;
+
+    //shallow copy method
+    public Shallow(int[] values){
+        data = values;
+    }
+
+    public void showData() {
+        System.out.println(Arrays.toString(data));
+    }
+
+    public static void main(String[] args) {
+        int[] values = {22,2,4};
+        Shallow shallowCopy = new Shallow(values);
+        shallowCopy.showData();
+        values[0] = 13;
+        shallowCopy.showData();
+    }
+
+
+
+
+
+
+
+
+
+
+//    //instantiate a int data array variable
+//   static private int data[];
+//
+//    // actually shallow copy method
+//   public Shallow(int[] values) {
+//        data = values;
+//    }
+//
+//
+//    //method to show data
+//    public void showData() {
+//        System.out.println(Arrays.toString(data));
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] values = {1,2,3};
+//        Shallow shallowCopy = new Shallow(values);
+//        shallowCopy.showData();
+//        values[0] = 13;
+//        shallowCopy.showData();
+//
+//    }
+
+
+
+/*
+
 
     //Shallow copy example
 
@@ -32,4 +92,6 @@ public class Shallow {
         shallowCopy.showData();
 
     }
+
+ */
  }
